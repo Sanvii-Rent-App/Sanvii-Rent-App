006243421d2fa2204a866104eb413e77756a9aa4
Sohail <msksohailk@gmail.com>
2024-08-22 18:59:30 +0530

    added Port Number in application.propertiesgit

--- src/main/java/com/rent_mitra/payment_service/controller/PaymentServiceTestController.java
+++ src/main/java/com/rent_mitra/payment_service/controller/PaymentServiceTestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment-service")
+@RequestMapping("/pay")
 public class PaymentServiceTestController {
 
     @GetMapping("/get-name")
