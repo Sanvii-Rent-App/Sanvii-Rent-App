16a2299b34ce4f8c518198b0465d418a38cf1ded
Harsh <harsh.shivre@gmail.com>
2024-08-22 12:23:54 +0530

    Added swagger dependency

--- src/main/java/com/rent_mitra/payment_service/controller/PaymentServiceTestController.java
+++ src/main/java/com/rent_mitra/payment_service/controller/PaymentServiceTestController.java
@@ -2,15 +2,15 @@ package com.rent_mitra.payment_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment-service")
 public class PaymentServiceTestController {
 
-    @GetMapping
-    public String testPaymentService(){
-
-        return "This is payment service test controller";
+    @GetMapping("/get-name")
+    public String testPaymentService(@RequestParam String name){
+        return "This is payment service test controller and your name is := "+ name;
     }
 }
